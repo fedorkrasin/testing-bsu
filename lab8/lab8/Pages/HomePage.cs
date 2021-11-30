b4fd717bb2663d43b1b240c213cd571551547f14
fedor.krasin <fedor.krasin@itechart-group.com>
2021-11-30 18:45:17 +0300

    Installed other selenium packages. Created class of page of the site

---
+++ lab8/lab8/Pages/HomePage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace lab8.Pages
+{
+    public class HomePage
+    {
+        
+    }
+}
---
+++ lab8/lab8/Pages/Page.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace lab8.Pages
+{
+    public class Page
+    {
+        public Page(IWebDriver webDriver, string entryUrl)
+        {
+            this.WebDriver = webDriver;
+            this.EntryUrl = entryUrl;
+            PageFactory.InitElements(webDriver, this);
+        }
+
+        protected IWebDriver WebDriver { get; }
+
+        protected string EntryUrl { get; }
+
+        public string CurrentUrl => this.WebDriver.Url;
+
+        public virtual Page OpenPage()
+        {
+            this.WebDriver.Navigate().GoToUrl(this.EntryUrl);
+
+            return this;
+        }
+    }
+}
