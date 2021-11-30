aab3c6ec40e221a4788e712a66998f576cc1a2b8
fedor.krasin <fedor.krasin@itechart-group.com>
2021-11-30 17:54:12 +0300

    Created lab8. Added .gitignore

---
+++ lab8/lab8/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace lab8
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
