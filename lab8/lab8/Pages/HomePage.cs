2df349b3114dd0a5973a818b9e5f31554327817a
fedor.krasin <fedor.krasin@itechart-group.com>
2021-11-30 19:15:30 +0300

    Added methods and fields for managing page

--- lab8/lab8/Pages/HomePage.cs
+++ lab8/lab8/Pages/HomePage.cs
@@ -4,8 +4,51 @@ using OpenQA.Selenium.Support.UI;
 
 namespace lab8.Pages
 {
-    public class HomePage
+    public class HomePage : Page
     {
+        public HomePage(IWebDriver webDriver) : base(webDriver, "https://poezdato.net/") { }
         
+        public IWebElement FromPlaceField => FindBy(By.Id("dir_from"));
+        public IWebElement ToPlaceField => FindBy(By.Id("dir_where"));
+        public IWebElement DateField => FindBy(By.Id("dir_date"));
+        public IWebElement FindButton => FindBy(By.Id("dir_submit"));
+        
+        public HomePage EnterFromPlace(string place)
+        {
+            FromPlaceField.SendKeys(place);
+
+            return this;
+        }
+
+        public HomePage EnterToPlace(string place)
+        {
+            ToPlaceField.SendKeys(place);
+
+            return this;
+        }
+
+        public HomePage SelectDate(string date)
+        {
+            DateField.SendKeys(date);
+
+            return this;
+        }
+        
+        private IWebElement FindBy(By key)
+        {
+            return new WebDriverWait(WebDriver, TimeSpan.FromSeconds(5)).Until(driver => driver.FindElement(key));
+        }
+
+        public void SearchTrips()
+        {
+            FindButton.Click();
+        }
+        
+        public override HomePage OpenPage()
+        {
+            WebDriver.Navigate().GoToUrl(EntryUrl);
+
+            return this;
+        }
     }
 }
--- lab8/lab8/PoezdatoTests.cs
+++ lab8/lab8/PoezdatoTests.cs
@@ -1,17 +1,29 @@
+using System.Threading;
+using lab8.Pages;
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace lab8
 {
+    [TestFixture]
     public class Tests
     {
+        private IWebDriver _driver;
+        
         [SetUp]
-        public void Setup()
+        public void InitDriver()
         {
+            var option = new ChromeOptions();
+            option.AddArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+            _driver = new ChromeDriver(option);
         }
 
         [Test]
-        public void Test1()
+        public void SearchTrainsTest()
         {
+            var homePage = new HomePage(_driver).OpenPage();
+            
             Assert.Pass();
         }
     }
