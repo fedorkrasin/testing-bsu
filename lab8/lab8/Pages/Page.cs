c338f6adb433a1217b504b0a73fc5e1239e435d3
fedor.krasin <fedor.krasin@itechart-group.com>
2021-11-30 20:01:17 +0300

    Added lab8 with right .gitignore

---
+++ lab8/lab8/Pages/Page.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace lab8.Pages
+{
+    public class Page
+    {
+        public Page(IWebDriver webDriver, string entryUrl)
+        {
+            WebDriver = webDriver;
+            EntryUrl = entryUrl;
+            PageFactory.InitElements(webDriver, this);
+        }
+
+        protected IWebDriver WebDriver { get; }
+
+        protected string EntryUrl { get; }
+
+        public string CurrentUrl => WebDriver.Url;
+
+        public virtual Page OpenPage()
+        {
+            WebDriver.Navigate().GoToUrl(EntryUrl);
+
+            return this;
+        }
+    }
+}
---
+++ lab8/lab8/PoezdatoTests.cs
@@ -0,0 +1,45 @@
+using System.Threading;
+using lab8.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace lab8
+{
+    [TestFixture]
+    public class Tests
+    {
+        private IWebDriver _driver;
+        
+        [SetUp]
+        public void InitDriver()
+        {
+            var option = new ChromeOptions();
+            option.AddArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+            _driver = new ChromeDriver(option);
+        }
+
+        [Test]
+        public void SearchTrainsTest()
+        {
+            var homePage = new HomePage(_driver).OpenPage();
+            
+            homePage.EnterFromPlace("Minsk")
+                .EnterToPlace("Baranovichi")
+                .SelectDate("16.12.2021")
+                .CloseDateField()
+                .SearchTrips();
+
+            var expectedPageUrl = "https://poezdato.net/raspisanie-poezdov/minsk--baranovichi/16.12.2021/";
+            var expectedPage = new Page(_driver, expectedPageUrl).OpenPage();
+            
+            Assert.AreEqual(homePage.CurrentUrl, expectedPage.CurrentUrl);
+        }
+        
+        [TearDown]
+        public void CloseDriver()
+        {
+            _driver.Close();
+        }
+    }
+}
