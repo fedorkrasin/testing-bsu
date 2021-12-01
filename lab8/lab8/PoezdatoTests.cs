231fdf1c0f38618eda51b988ca63d09440e64f42
fedor.krasin <fedor.krasin@itechart-group.com>
2021-12-01 11:46:00 +0300

    Code changes

--- lab8/lab8/PoezdatoTests.cs
+++ lab8/lab8/PoezdatoTests.cs
@@ -17,7 +17,7 @@ namespace lab8
         public void InitDriver()
         {
             var option = new ChromeOptions();
-            option.AddArguments("--window-size=1920,937");
+            option.AddArguments("--headless", "--window-size=1920,937");
             _driver = new ChromeDriver(option);
             
             _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
